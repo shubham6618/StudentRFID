deadbadff0807761e6395c9e245505617ff9484c
Shubham Sharma <shubhamsharma6618@live.co.uk>
2016-05-28 00:08:12 +0530

    minor file added

--- WebContent/statecity.js
+++ WebContent/statecity.js
@@ -1,33 +1,25 @@
  $(document).ready(
         function() {
-        		
-        	
-        	
-        	
-        	
-        	
-        	
-        	
-        	
-
-        	 $.getJSON("FetchStatesJSON", function(json){
+       	 $.getJSON("FetchStatesJSON", function(json){
                  $('#stateid').empty();
                  $('#stateid').append($('<option>').text("Select"));
                  $.each(json, function(i, item){
-                         $('#stateid').append($('<option>').text(item.STATENAME).attr('value', item.STATEID));
+                         $('#stateid').append($('<option>').text(item.STATENAME).attr('value', item.STATENAME).attr('myid',item.STATEID));
                  });
              });
         	 
        
  
  $("#stateid").change(function(){
-	 
- 	$.getJSON("FetchAllCitiesByStateIdJSON",{sid:$('#stateid').val()}, function(json){
- 	
+	 var element = $(this).find('option:selected'); 
+     var myTag = element.attr("myTag"); 
+
+ 	$.getJSON("FetchAllCitiesByStateIdJSON",{sid:$('#stateid option:selected',this).attr('myid')}, function(json){
+ 			console.log(sid);
          $('#cityid').empty();
          $('#cityid').append($('<option>').text("Select"));
          $.each(json, function(i, item){
-                 $('#cityid').append($('<option>').text(item.CITYNAME).attr('value', item.CITYID));
+                 $('#cityid').append($('<option>').text(item.CITYNAME).attr('value', item.CITYNAME));
          	});
  		}); 
  	});
